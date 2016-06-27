122a7089367e9fd739cf70262967411aa9b993cd
alextsil <alex19924@gmail.com>
2016-06-27 12:10:36 +0300

    Ticket status json now gets parsed to enum on creation

--- admin/app/in-memory-data.service.ts
+++ admin/app/in-memory-data.service.ts
@@ -1,16 +1,16 @@
 export class InMemoryDataService {
     createDb() {
         let tickets = [
-            {"id": 1, "message": "Mr. Nice", "status": "New", "datetime": "2016-04-17 13:10:55"},
-            {"id": 2, "message": "Narco", "status": "New", "datetime": "2016-04-17 13:10:55"},
-            {"id": 3, "message": "Bombasto", "status": "New", "datetime": "2016-04-17 13:10:55"},
-            {"id": 4, "message": "Celeritas", "status": "New", "datetime": "2016-04-17 13:10:55"},
-            {"id": 5, "message": "Magneta", "status": "New", "datetime": "2016-04-17 13:10:55"},
-            {"id": 6, "message": "RubberMan", "status": "New", "datetime": "2016-04-17 13:10:55"},
-            {"id": 7, "message": "Dynama", "status": "New", "datetime": "2016-04-17 13:10:55"},
-            {"id": 8, "message": "Dr IQ", "status": "New", "datetime": "2016-04-17 13:10:55"},
-            {"id": 9, "message": "Magma", "status": "New", "datetime": "2016-04-17 13:10:55"},
-            {"id": 10, "message": "Tornado", "status": "Assigned", "datetime": "2016-04-17 13:10:55"}
+            {"id": 1, "message": "Mr. Nice", "status": "NEW", "datetime": "2016-04-17 13:10:55"},
+            {"id": 2, "message": "Narco", "status": "NEW", "datetime": "2016-04-17 13:10:55"},
+            {"id": 3, "message": "Bombasto", "status": "NEW", "datetime": "2016-04-17 13:10:55"},
+            {"id": 4, "message": "Celeritas", "status": "NEW", "datetime": "2016-04-17 13:10:55"},
+            {"id": 5, "message": "Magneta", "status": "NEW", "datetime": "2016-04-17 13:10:55"},
+            {"id": 6, "message": "RubberMan", "status": "NEW", "datetime": "2016-04-17 13:10:55"},
+            {"id": 7, "message": "Dynama", "status": "NEW", "datetime": "2016-04-17 13:10:55"},
+            {"id": 8, "message": "Dr IQ", "status": "NEW", "datetime": "2016-04-17 13:10:55"},
+            {"id": 9, "message": "Magma", "status": "NEW", "datetime": "2016-04-17 13:10:55"},
+            {"id": 10, "message": "Tornado", "status": "ASSIGNED", "datetime": "2016-04-17 13:10:55"}
         ];
         return {tickets};
     }
--- admin/app/ticket.ts
+++ admin/app/ticket.ts
@@ -1,18 +1,54 @@
 export class Ticket {
     id:number;
     message:string;
-    status:string; //Should be kept in sync with the TickeStatus enum in the backend
+    status:TicketStatus; //Should be kept in sync with the TickeStatus enum in the backend
     datetime:Date;
-
 
     constructor(id:number, message:string, status:string, datetime:string) {
         this.id = id;
         this.message = message;
-        this.status = status;
+        this.status = this.getStatusFromString(status);
         this.datetime = new Date(datetime);
     }
 
     static fromJSONArray(array:Array<Object>):Ticket[] {
         return array.map(obj => new Ticket(obj['id'], obj['message'], obj['status'], obj['datetime']));
     }
+
+    static fromJSON(ticket:Object):Ticket {
+        return new Ticket(ticket['id'], ticket['message'], ticket['status'], ticket['datetime']);
+    }
+
+    private getStatusFromString(status:string):TicketStatus {
+        console.info("Parsing json status " + status + " to enum");
+        let ticketStatus;
+        if (status == "NEW") {
+            ticketStatus = TicketStatus.NEW;
+        } else if (status == "REJECTED") {
+            ticketStatus = TicketStatus.REJECTED;
+        } else if (status == "ASSIGNED") {
+            ticketStatus = TicketStatus.ASSIGNED;
+        } else if (status == "IN_PROGRESS") {
+            ticketStatus = TicketStatus.IN_PROGRESS;
+        } else if (status == "ABORTED") {
+            ticketStatus = TicketStatus.ABORTED;
+        } else if (status == "DONE") {
+            ticketStatus = TicketStatus.DONE;
+        } else {
+            console.error("Cannot parse ticket status " + status + " from json");
+            throw new TypeError("Json status " + status + " doesn't match any enum candidates");
+        }
+        console.info("Json status parsed to enum : " + ticketStatus);
+        return ticketStatus;
+    }
+
+}
+
+enum TicketStatus{
+    NEW = <any>"New",
+    REJECTED = <any>"Rejected",
+    ASSIGNED = <any>"Assigned",
+    IN_PROGRESS = <any>"In progress",
+    ABORTED = <any>"Aborted",
+    DONE = <any>"Done"
 }
