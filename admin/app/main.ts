8bb80395817691824d58de39bce9a5c58e2d3d2a
alextsil <alex19924@gmail.com>
2016-06-28 19:43:59 +0300

    production mode enabled - ng-select added on template

--- admin/app/main.ts
+++ admin/app/main.ts
@@ -1,16 +1,19 @@
 import {bootstrap} from "../node_modules/@angular/platform-browser-dynamic";
 import {HTTP_PROVIDERS, XHRBackend} from "@angular/http";
-import {provide} from "@angular/core";
+import {provide, enableProdMode} from "@angular/core";
 import {AppComponent} from "./app.component";
 import {InMemoryBackendService, SEED_DATA} from "angular2-in-memory-web-api";
 import {InMemoryDataService} from "./in-memory-data.service";
 import {APP_ROUTER_PROVIDERS} from "./routes";
 import {disableDeprecatedForms, provideForms} from "@angular/forms";
 
+enableProdMode();
 
 bootstrap(AppComponent, [HTTP_PROVIDERS,
     provide(XHRBackend, {useClass: InMemoryBackendService}), // in-mem server
     provide(SEED_DATA, {useClass: InMemoryDataService}),  // in-mem server data]);
     APP_ROUTER_PROVIDERS,
     disableDeprecatedForms(),
-    provideForms()]).catch(err => console.error(err));
+    provideForms()])
+    .catch(err => console.error(err));
+
--- admin/app/ticket-detail.component.ts
+++ admin/app/ticket-detail.component.ts
@@ -34,4 +34,36 @@ export class TicketDetailComponent implements OnInit {
     goToTickets() {
         this.router.navigate(['/tickets']);
     }
+
+    //ng2-select stuff
+    public items:Array<string> = ['Amsterdam', 'Antwerp', 'Athens', 'Barcelona',
+        'Berlin', 'Birmingham', 'Bradford', 'Bremen', 'Brussels', 'Bucharest',
+        'Budapest', 'Cologne', 'Copenhagen', 'Dortmund', 'Dresden', 'Dublin', 'Düsseldorf',
+        'Essen', 'Frankfurt', 'Genoa', 'Glasgow', 'Gothenburg', 'Hamburg', 'Hannover',
+        'Helsinki', 'Leeds', 'Leipzig', 'Lisbon', 'Łódź', 'London', 'Kraków', 'Madrid',
+        'Málaga', 'Manchester', 'Marseille', 'Milan', 'Munich', 'Naples', 'Palermo',
+        'Paris', 'Poznań', 'Prague', 'Riga', 'Rome', 'Rotterdam', 'Seville', 'Sheffield',
+        'Sofia', 'Stockholm', 'Stuttgart', 'The Hague', 'Turin', 'Valencia', 'Vienna',
+        'Vilnius', 'Warsaw', 'Wrocław', 'Zagreb', 'Zaragoza'];
+
+    private value:any = ['Athens', 'Amsterdam'];
+
+    public selected(value:any):void {
+        console.log('Selected value is: ', value);
+    }
+
+    public removed(value:any):void {
+        console.log('Removed value is: ', value);
+    }
+
+    public refreshValue(value:any):void {
+        this.value = value;
+    }
+
+    public itemsToString(value:Array<any> = []):string {
+        return value
+            .map((item:any) => {
+                return item.text;
+            }).join(',');
+    }
 }
