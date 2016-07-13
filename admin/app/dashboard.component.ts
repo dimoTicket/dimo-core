c36ee4a89c71c74d6cf0ad8408cf51a9e9df214c
alextsil <alex19924@gmail.com>
2016-07-13 10:14:49 +0300

    ^

--- admin/app/dashboard.component.ts
+++
@@ -1,9 +0,0 @@
-import {Component} from "@angular/core";
-
-@Component({
-    selector: 'my-dashboard',
-    templateUrl: 'app/templates/dashboard.component.html'
-})
-export class DashboardComponent {
-}
-
